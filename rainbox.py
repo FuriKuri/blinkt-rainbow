8d4727cc7e64f910f670a2a2dbf209c9e995a7b7
Theo Pack <tf.pack@gmail.com>
2017-12-23 10:19:01 +0100

    Add graceful sig term

--- rainbox.py
+++ rainbox.py
@@ -5,19 +5,33 @@ import time
 
 import blinkt
 
+class GracefulKiller:
+  kill_now = False
+  def __init__(self):
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+  def exit_gracefully(self,signum, frame):
+    self.kill_now = True
+
 spacing = 360.0 / 16.0
 hue = 0
 
 blinkt.set_clear_on_exit()
 blinkt.set_brightness(0.1)
 
-while True:
-    hue = int(time.time() * 100) % 360
-    for x in range(blinkt.NUM_PIXELS):
-        offset = x * spacing
-        h = ((hue + offset) % 360) / 360.0
-        r, g, b = [int(c*255) for c in colorsys.hsv_to_rgb(h, 1.0, 1.0)]
-        blinkt.set_pixel(x, r, g, b)
+killer = GracefulKiller()
 
-    blinkt.show()
-    time.sleep(0.001)
+while True:
+  hue = int(time.time() * 100) % 360
+  for x in range(blinkt.NUM_PIXELS):
+      offset = x * spacing
+      h = ((hue + offset) % 360) / 360.0
+      r, g, b = [int(c*255) for c in colorsys.hsv_to_rgb(h, 1.0, 1.0)]
+      blinkt.set_pixel(x, r, g, b)
+
+  blinkt.show()
+  time.sleep(0.001)
+  if killer.kill_now:
+    blinkt.clear()
+    break
